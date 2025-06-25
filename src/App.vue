11805d6f7ea044572ae5836c7ba38e7d4aa0f625
dev1joe <yousef.m.dev1@gmail.com>
2025-06-26 01:33:46 +0300

    modifications

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,26 @@ const photo = ref('');
 const selectedLocation = ref('London');
 const dayCount = ref(7);
 
+
+/* TODO: Celsius VS Fahrenheit
+* get data from api result
+* extract celsius data in data_c and fahrenheit data in data_f
+* set data to data_c automatically
+* add C and F buttons
+* add event listener to C and F buttons
+* onclick switch data to either data_c or data_f
+*/
+
+// TODO: weather evaluation e.g. average UV index, low visibility, etc..
+// TODO: add a loading state
+// TODO: custom icons
+
+
 async function fetchWeather() {
   console.log('Fetching weather data for:', selectedLocation.value);
 
   let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHER_API}&q=${selectedLocation.value}&days=${dayCount.value}`);
-  
+
   if (response.ok) {
     let json = await response.json();
     data.value = json;
@@ -48,43 +63,92 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="main-container w-dvw h-dvh grid grid-cols-[1fr_4fr]">
+  <!-- TODO: add dark/light mode switch -->
+  <div class="main-container w-dvw h-dvh grid grid-cols-[1fr_3fr]">
 
-    <Sidebar :data="data" :photo="photo" :location="selectedLocation" />
+    <!-- TODO: pass only pieces of information not the whole data object  -->
+    <Sidebar :data="data" :photo="photo" :location="selectedLocation">
+      <input type="text"
+        class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-2xl dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
+        placeholder="e.g. London" v-model="selectedLocation" @input="fetchWeather" />
+    </Sidebar>
 
-    <div class="right-content bg-gray-100 dark:bg-gray-700 p-4">
+    <div class="right-content bg-gray-100 dark:bg-gray-700 px-8">
 
-      <div class="upper my-4">
-        <input type="text"
-          class="w-full mb-4 py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
-          placeholder="e.g. London" v-model="selectedLocation" @input="fetchWeather" />
+      <!-- TODO: include a tab for today's timeline -->
+      <div class="upper mt-4 grid grid-cols-[1fr_3fr]">
+        <h1 class="mb-4 ms-4 text-3xl font-bold capitalize">this week</h1>
+
+        <div class="ms-auto">
+          <button type="button" class="bg-black rounded-4xl size-8 text-center ">&deg;C</button>
+          <button type="button" class="!bg-white !text-black rounded-4xl size-8 text-center ">&deg;F</button>
+        </div>
       </div>
 
       <!-- Cards Section -->
       <!-- TODO: convert to card component -->
-      <div class="week-weather mb-4">
-        <h1 class="mb-4 ms-4 text-3xl font-bold capitalize">this week</h1>
+      <div class="week-weather mb-6">
         <div class="grid grid-cols-7 gap-2">
-          <div class="card bg-white dark:bg-gray-900 rounded-3xl text-sm" v-for="(day, index) in data.forecast?.forecastday"
-            :key="index">
+          <div class="day-card p-4 bg-white dark:bg-gray-900 rounded-3xl text-sm text-center"
+            v-for="(day, index) in data.forecast?.forecastday" :key="index">
             <p class="mb-2">{{ day.date }}</p>
-            <img class="mb-2" :src="day.day.condition.icon" alt="condition icon">
+            <img class="mb-2 mx-auto" :src="day.day.condition.icon" alt="condition icon">
             <p>
-              <span>{{ day.day.maxtemp_c }}&deg</span>
-              <span class="ms-1 text-gray-400">{{ day.day.mintemp_c }}&deg</span>
+              <span>{{ Math.floor(day.day.maxtemp_c) }}&deg;</span>
+              <span class="ms-1 text-gray-400">{{ Math.floor(day.day.mintemp_c) }}&deg;</span>
             </p>
           </div>
         </div>
       </div>
 
-      <!-- TODO: Highlights section -->
-      <div class="highlights mb-4">
+      <!-- Highlights section -->
+      <div class="highlights">
         <h1 class="mb-4 ms-4 text-3xl font-bold capitalize">Today's Highlights</h1>
-        <div class="grid grid grid-cols-3 gap-4">
-          <div class="bg-white dark:bg-gray-900 rounded-3xl ">
+
+        <!-- cards -->
+        <div class="grid grid-cols-3 gap-4">
+          <div class="p-4 bg-white dark:bg-gray-900 rounded-3xl ">
             <p class="text-gray-400 capitalize mb-4">wind status</p>
-            <h1>{{ data.current?.wind_mph }}</h1>
+            <p class="mb-4"><span class="text-6xl">{{ data.current?.wind_mph }}</span> km/h</p>
+            <p>{{ data.current?.wind_dir }}</p>
           </div>
+
+          <div class="p-4 bg-white dark:bg-gray-900 rounded-3xl ">
+            <p class="text-gray-400 capitalize mb-4">Humidity</p>
+            <p class="mb-4"><span class="text-6xl">{{ data.current?.humidity }}</span> %</p>
+            <p>Normal</p>
+          </div>
+
+          <div class="p-4 bg-white dark:bg-gray-900 rounded-3xl ">
+            <p class="text-gray-400 capitalize mb-4">Sunrise & Sunset</p>
+            
+            <div class="h-10 flex items-center mb-4">
+              <div class="inline-block size-9 rounded-4xl text-center me-2 text-3xl bg-gray-100 dark:bg-gray-700">&uarr;</div>
+              <span class="text-xl">{{ data.forecast?.forecastday[0].astro.sunrise }}</span>
+            </div>
+            
+            <div class="h-10 flex items-center mb-4">
+              <div class="inline-block size-9 rounded-4xl text-center me-2 text-3xl bg-gray-100 dark:bg-gray-700">&darr;</div>
+              <span class="text-xl">{{ data.forecast?.forecastday[0].astro.sunset }}</span>
+            </div>
+          </div>
+
+          <div class="p-4 bg-white dark:bg-gray-900 rounded-3xl ">
+            <p class="text-gray-400 capitalize mb-4">UV index</p>
+            <p class="text-6xl mb-4 text-center">{{ data.current?.uv }}</p>
+          </div>
+
+          <div class="p-4 bg-white dark:bg-gray-900 rounded-3xl ">
+            <p class="text-gray-400 capitalize mb-4">visibility</p>
+            <p class="mb-4"><span class="text-6xl">{{ data.current?.vis_km }}</span> km</p>
+            <p>Average</p>
+          </div>
+
+          <div class="p-4 bg-white dark:bg-gray-900 rounded-3xl ">
+            <p class="text-gray-400 capitalize mb-4">Chance of rain</p>
+            <p class="mb-4"><span class="text-6xl">{{ data.forecast?.forecastday[0].day.daily_chance_of_rain }}</span> %</p>
+          </div>
+
         </div>
       </div>
     </div>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -9,24 +9,39 @@ defineProps({
 </script>
 
 <template>
-    <div class="sidebar bg-white dark:bg-gray-900 h-full flex flex-col justify-between">
+    <div class="sidebar bg-white p-6 dark:bg-gray-900 h-full flex flex-col justify-between">
         <aside
-            class="flex flex-col px-7 overflow-y-auto bg-white rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
-            
-            <img class="w-8/10" :src="data.forecast?.forecastday[0].day.condition.icon" alt="condition icon">
+            class="flex flex-col items-center bg-white rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
+
+            <slot />
+
+            <img class="w-8/10 scale-125" :src="data.current?.condition.icon" alt="condition icon">
 
             <div class="flex content-start pb-4">
                 <p class="text-7xl">{{ data.current?.temp_c }}</p>
                 <p class="text-4xl">&degC</p>
             </div>
 
-            <p class="mb-4">Current Date, 12:00 pm</p>
+            <div class="w-full px-4 text-xl">
+                <img :src="data.forecast?.forecastday[0].day.condition.icon" alt="" class="inline">
+                <span>{{ data.forecast?.forecastday[0].day.condition.text }}</span>
+            </div>
 
-            <hr class="mb-4 border-gray-200 dark:border-gray-600" />
+            <div class="w-full px-4 mb-2 text-xl">
+                <span class="me-2">🌡️</span>
+                <span class="me-2">H: {{ Math.floor(data.forecast?.forecastday[0].day.maxtemp_c) }} &deg</span>
+                <span>L: {{ Math.floor(data.forecast?.forecastday[0].day.mintemp_c) }}&deg</span>
+            </div>
+
+            <div class="w-full px-4 mb-2 text-xl">
+                <span>⏰</span>
+                <span class="me-2">Tuesday</span>
+                <span>12:00</span>
+            </div>
 
             <!-- :src="data.forecast.forecastday[0].day.condition.icon" -->
 
-            <div class="flex items-center mb-4">
+            <!-- <div class="flex items-center mb-4">
                 <img class="me-2" :src="data.current?.condition.icon" alt="">
                 <p>{{ data.forecast?.forecastday[0].day.condition.text }}</p>
             </div>
@@ -34,13 +49,14 @@ defineProps({
             <div class="flex items-center mb-4">
                 <img class="me-2" :src="data.current?.condition.icon" alt="">
                 <p>Rain - {{ data.forecast?.forecastday[0].day.daily_chance_of_rain }}%</p>
-            </div>
+            </div> -->
 
         </aside>
+        <hr class="mb-4 border-gray-200 dark:border-gray-600" />
         <div
-            class="flex justify-center items-center py-10 px-6 rounded-4xl bg-lime-700 bg-cover m-4"
+            class="flex justify-center items-center mx-4 py-10 px-15 rounded-4xl bg-lime-700 bg-cover text-center"
             :style="{ backgroundImage: `url(${photo})` }">
-            <p class="font-bold">{{ data.location?.name }}</p>
+            <p class="font-bold">{{ data.location?.region }}</p>
         </div>
     </div>
 
